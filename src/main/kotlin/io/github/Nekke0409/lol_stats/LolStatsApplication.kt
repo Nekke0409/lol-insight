bd670b409dc5358573b25700fc9fcd2efc074532
SeongHeon Kim <sheon2001@gmail.com>
2026-09-02 17:30:56 +0900

    chore: initial project

---
+++ src/main/kotlin/io/github/Nekke0409/lol_stats/LolStatsApplication.kt
@@ -0,0 +1,11 @@
+package io.github.Nekke0409.lol_stats
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LolStatsApplication
+
+fun main(args: Array<String>) {
+	runApplication<LolStatsApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/Nekke0409/lol_stats/LolStatsApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.Nekke0409.lol_stats
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LolStatsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
